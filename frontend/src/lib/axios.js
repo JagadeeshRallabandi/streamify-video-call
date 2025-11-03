d51661fba62f28e66b119400a73208c77647effa
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2025-11-04 01:37:45 +0530

    for sure

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =  "https://jagastreamifybackend.vercel.app/api/";
+const BASE_URL =  "https://jagastreamifybackend.vercel.app/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
